7a404c45df143a65edfacfd79315eb6fd64efede
mamfyou <mamf20021381@gmail.com>
2024-02-06 13:23:47 +0330

    adding LICENSE and README.md

--- RestaurantAccountancy/settings.py
+++ RestaurantAccountancy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['restaurant.codinto.ir', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
 # Application definition
 
